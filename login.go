f57be3cb866f8f139808c88850b9ff0f419060c8
ryoma kawahara <akahirefriend2008@yahoo.com>
2019-06-14 09:39:30 +0900

    study duplicate send

--- login.go
+++ login.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
 	"text/template"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -24,12 +28,25 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 func login(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("method:", r.Method)
 	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
 		t, _ := template.ParseFiles("login.gptl")
-		t.Execute(w, nil)
+		t.Execute(w, token)
 	} else {
+		//リクエストはログインデータです。ログインのロジックを実行して判断します。
 		r.ParseForm()
-		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		token := r.Form.Get("token")
+		if token != "" {
+			//tokenの合法性を検証します。
+		} else {
+			//tokenが存在しなければエラーを出します。
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //サーバ側に出力します。
 		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //クライアントに出力します。
 	}
 }
 
